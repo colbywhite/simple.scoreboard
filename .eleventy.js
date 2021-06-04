c3b25db5a70da86f08efea48372a1d7ba35ff0ff
Colby M. White <colbywhite@users.noreply.github.com>
2021-06-03 22:05:22 -0500

    feat: use imgs instead of inline data

--- .eleventy.js
+++ .eleventy.js
@@ -1,4 +1,4 @@
 module.exports = function(eleventyConfig) {
   eleventyConfig.addPassthroughCopy('index.css');
-  eleventyConfig.addPassthroughCopy('/logos');
+  eleventyConfig.addPassthroughCopy('logos');
 };
