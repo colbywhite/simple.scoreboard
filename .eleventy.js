d355b34853991680596f856c7a4f2cf8ee1202cf
Colby M. White <colbywhite@users.noreply.github.com>
2021-06-05 15:45:54 -0500

    refactor: auto-generate logo css classes

--- .eleventy.js
+++ .eleventy.js
@@ -1,4 +1,9 @@
-module.exports = function(eleventyConfig) {
-  eleventyConfig.addPassthroughCopy('index.css');
+const fs = require('fs');
+
+module.exports = function (eleventyConfig) {
   eleventyConfig.addPassthroughCopy('logos');
+  eleventyConfig.addCollection('logos', _api => {
+    const regex = /(.*)\.png/;
+    return fs.readdirSync('logos').map(logo => ({path: `logos/${logo}`, name: logo.match(regex)[1]}));
+  });
 };
