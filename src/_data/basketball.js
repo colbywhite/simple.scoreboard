9b18cfe1ebc1cbe852348bdb1ce59ad8c727a71a
Colby M. White <colbywhite@users.noreply.github.com>
2021-10-26 15:05:43 -0500

    feat: add nba power rankings

--- src/_data/basketball.js
+++ src/_data/basketball.js
@@ -1,9 +1,11 @@
 const Cache = require('@11ty/eleventy-cache-assets');
 const utils = require('../_utils/utils');
 const { DateTime } = require('luxon');
+const cheerio = require("cheerio");
 
 const SPORT = 'basketball';
 const NBA_URL = 'https://data.nba.com/data/10s/v2015/json/mobile_teams/nba/2021/league/00_full_schedule.json';
+// const WNBA_URL = 'https://data.wnba.com/data/10s/v2015/json/mobile_teams/wnba/2021/league/10_full_schedule.json';
 /**
  * Since the feed doesn't distinguish preseason games,
  * all games before this date are considered preseason and thus ignored.
@@ -14,12 +16,22 @@ function getNBASchedule() {
   return Cache(NBA_URL, {duration: '1d', type: 'json'});
 }
 
+function getRankings() {
+    return Cache('http://www.powerrankingsguru.com/nba/team-power-rankings.php', {
+        duration: '1d',
+        type: 'text'
+    })
+        .then(cheerio.load)
+        .then(parser => parser('table.rankings-table > tbody > tr > td:nth-child(2) > div.team-name-v2 > span.full-name'))
+        .then(spanNodes => spanNodes.toArray().map(d => d.children[0].data));
+}
+
 function isRegularSeason(game) {
     const gameDate = DateTime.fromISO(game.date.toISOString());
     return gameDate > REGULAR_SEASON_START_DATE;
 }
 
-function parseRawGames(games) {
+function parseRawGames(games, rankings) {
   return games.lscd.reduce((games, currentMonth) => {
     const parsedMonth = currentMonth.mscd.g
       .map(game => ({
@@ -33,6 +45,7 @@ function parseRawGames(games) {
           nickname: game.h.tn,
           city: game.h.tc,
           logoClass: `${SPORT}-${game.h.ta.toLowerCase()}`,
+          rank: findTeamRank(game.h.tn, rankings),
           sport: SPORT
         },
         away: {
@@ -40,6 +53,7 @@ function parseRawGames(games) {
           nickname: game.v.tn,
           city: game.v.tc,
           logoClass: `${SPORT}-${game.v.ta.toLowerCase()}`,
+          rank: findTeamRank(game.v.tn, rankings),
           sport: SPORT
         },
         date: (game.stt === 'TBD') ? new Date(`${game.gdte}T19:00:00-0400`) : new Date(`${game.etm}-0400`)
@@ -62,7 +76,13 @@ function parseStatus(game) {
   }
 }
 
-const getGames = getNBASchedule().then(parseRawGames);
+function findTeamRank(teamName, rankings) {
+    const zeroIndexedRank = rankings.findIndex(name => teamName.toLowerCase() === name.toLowerCase());
+    return zeroIndexedRank + 1;
+}
+
+const getGames = Promise.all([getNBASchedule(), getRankings()])
+    .then(([schedule, rankings]) => parseRawGames(schedule, rankings));
 const getTeams = getGames.then(utils.parseTeams)
 module.exports = Promise.all([getGames, getTeams])
   .then(([games, teams]) => {
